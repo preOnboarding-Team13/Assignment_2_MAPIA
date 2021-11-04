5f9090cc65f70547f111fbd48adfb4f7cc593f14
luckyhyom <bs_khm@naver.com>
2021-11-05 01:44:05 +0900

    Test: controller test

--- src/domain/song/song.controller.spec.ts
+++ src/domain/song/song.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { SongController } from "./song.controller";
+import { SongService } from "./song.service";
+
+const mockService = {
+	createOne: jest.fn()
+};
 
 describe("SongController", () => {
 	let controller: SongController;
 
 	beforeEach(async () => {
 		const module: TestingModule = await Test.createTestingModule({
-			controllers: [SongController]
+			controllers: [SongController],
+			providers: [
+				{
+					provide: SongService,
+					useValue: mockService
+				}
+			]
 		}).compile();
-
 		controller = module.get<SongController>(SongController);
 	});
-
 	it("should be defined", () => {
 		expect(controller).toBeDefined();
+	});
+
+	it("should.. ", async () => {
+		const result = await controller.create({
+			name: "123",
+			genre: "asd",
+			runningTime: 213
+		});
+
+		console.log(result, "테스트!");
+
+		await controller
+			.create({
+				name: "123",
+				genre: "asd",
+				runningTime: 213
+			})
+			.catch((error) => {
+				console.log(error, "에러");
+				expect(error.message).toBe(
+					"Cannot read property 'records' of undefined"
+				);
+			});
 	});
 });
