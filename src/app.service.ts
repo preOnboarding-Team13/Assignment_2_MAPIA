65837866beeab13d3cba1e8ee27c98ac5475fd0a
sally0226 <sally734595@gmail.com>
2021-11-05 23:31:15 +0900

    Remove: 필요 없는 코드, 주석 삭제
    - app.service에 neo4j 연결 테스트를 위해 작성했던 코드 삭제
    - musicianRead.resolver에 주석 처리된 코드 삭제

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,11 +6,6 @@ export class AppService {
 	constructor(private readonly neo4jService: Neo4jService) {}
 
 	async getHello(): Promise<string> {
-		const result = await this.neo4jService.write(
-			"create(m1:MUSICIAN {name: '아이유', company: 'SM'}) return m1",
-			{}
-		);
-		console.log(result);
 		return `Hello World!`;
 	}
 }
--- src/read/musicianRead.resolver.ts
+++ src/read/musicianRead.resolver.ts
@@ -1,9 +1,6 @@
 import { Args, Parent, Query, ResolveField, Resolver } from "@nestjs/graphql";
 import { Album, Musician, Song } from "src/read/graphql/graphql.schema";
 import { ReadService } from "./read.service";
-// import { Loader } from "nestjs-dataloader";
-// import * as DataLoader from "dataloader";
-import { HaveSongDataLoader } from "./loader/haveSongDataLoader.loader";
 
 @Resolver(() => Musician)
 export class MusicianRead {
@@ -19,22 +16,10 @@ export class MusicianRead {
 		return this.readService.readMusician(id);
 	}
 
-	// data-loader 전
 	@ResolveField(() => [Song])
 	song(@Parent() musician: Musician) {
 		return this.readService.readHaveSong(musician);
 	}
-
-	// @ResolveField(() => [Song])
-	// song(
-	// 	@Parent() musician: Musician,
-	// 	@Loader(HaveSongDataLoader.name)
-	// 	locationGroupDataLoader: DataLoader<any, any>
-	// ) {
-	// 	console.log("hello");
-	// 	console.log(musician);
-	// 	return locationGroupDataLoader.load(musician);
-	// }
 
 	@ResolveField(() => [Album])
 	album(@Parent() musician: Musician) {
